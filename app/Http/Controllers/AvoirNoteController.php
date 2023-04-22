8da2e53cb2599232c55ecfa1efc291ff16418d58
Soniath <soniathgbegonnoude@gmail.com>
2023-04-22 18:01:33 +0100

    Mise en place de la BDD

---
+++ app/Http/Controllers/AvoirNoteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAvoirNoteRequest;
+use App\Http\Requests\UpdateAvoirNoteRequest;
+use App\Models\AvoirNote;
+
+class AvoirNoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAvoirNoteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AvoirNote $avoirNote)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AvoirNote $avoirNote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAvoirNoteRequest $request, AvoirNote $avoirNote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AvoirNote $avoirNote)
+    {
+        //
+    }
+}
